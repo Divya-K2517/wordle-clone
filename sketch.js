1f51208445d15ec8b5f85c35ae344272eeecdbb8
Divya-K2517 <115424525+Divya-K2517@users.noreply.github.com>
2025-01-09 04:15:37 -0500

    still working on letter input

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
-let WORD_LENGTH = 6;
 let CELL_GAP = 10;
 let CELL_SIZE = 60;
 let guesses;
 let word = ''; //word for the player to guess
-
-let buttons = []; //list of tiles
+let letter_buttons = []; 
+let tile_buttons = []; //list of tiles/guesses
 let keyboard = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
@@ -25,6 +24,8 @@ function setup() { //runs once when the program starts
     ['', '', '', '', '', ''] ]
   current_turn = 0;
   current_letter = 0;
+  draw_board();
+  draw_keyboard();
 }
 function draw() { //called continously
   background(0);
@@ -34,8 +35,8 @@ function draw() { //called continously
    textSize(50);
    fill(255); //title color
    text("Wordle (dupe)", windowWidth/2, windowHeight/10);
-   draw_board();
-   draw_keyboard();
+   update_buttons();
+
 }
 
 async function loadwords() {
@@ -48,6 +49,7 @@ async function loadwords() {
         .map(word => word.toUpperCase());
 }
 
+
 function generate_word() {
     return window.list[Math.floor(Math.random() * (window.list.length))];
 }
@@ -56,12 +58,34 @@ function draw_board() {
     for (let j=0; j < guesses[i].length; j++) {
       //draw tile
       let button = createButton();
-      button.id = `button_${i}_${j}`;
+      button.id = `button${i}_${j}`;
       button.html(guesses[i][j]);
       button.size(40,40);
       button.position(j * 50 + windowWidth/2.5, i * 60 + windowHeight/6);
-      buttons.push(button);
+      tile_buttons.push(button);
     }
+  }
+}
+
+function handle_input(letter) {
+  if (letter == '⌫') {
+    guesses[current_turn][current_letter] = '';
+    current_letter --;
+    updateTileDisplay();
+  }
+  else {
+    guesses[current_turn][current_letter] = letter;
+    if (current_letter < 5) {
+      current_letter += 1;
+    }
+    updateTileDisplay();
+  } 
+}
+
+function updateTileDisplay () {
+  for (let j = 0; j < 6; j++) {
+    let tileIndex = current_turn * 6 + j;
+    tile_buttons[tileIndex].html(guesses[current_turn][j]);
   }
 }
 
@@ -73,22 +97,11 @@ function draw_keyboard() {
       letter_key.html(keyboard[i][j]);
       letter_key.size(60,50);
       letter_key.position(j * 60 + windowWidth/3.5 + (i%2)*30, i * 60 + windowHeight * (2.5/3.5));
-      
-      letter_key.mousePressed ( () => {
-        //console.log(letter_key.id);
-        guesses[current_turn][current_letter] = letter_key.id;
-        if (current_letter < 5) {
-          current_letter += 1;
-        } 
-      })
-      
-    
-    }
-  }
-  if (keyIsPressed) {
-    guesses[current_turn][current_letter] = key.toUpperCase();
-    if (current_letter < 5) {
-      current_letter += 1;
+
+      let letter = keyboard[i][j];
+      letter_key.mousePressed(() => handle_input(letter));
+
+      letter_buttons.push(letter_key);
     }
   }
 }
@@ -96,6 +109,4 @@ function draw_keyboard() {
 async function startGame () {
   await loadwords();
   word = generate_word(); //generates a word for player to guess
-
 }
-
